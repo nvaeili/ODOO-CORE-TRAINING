7654b4de7220c862307a27dc5a4b7b5e48625b73
nvaeili <salenivaniel@gmail.com>
2023-12-16 00:24:09 +0800

    Up to chapter 6

---
+++ addons/realestate_salen/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'summary': 'Module for managing real estate sales',
+    'description': 'This module helps in managing real estate sales within Odoo.',
+    'author': 'Ivaniel Salen',
+    'category': 'Sales/Real Estate',
+    'depends': ['base', 'sale'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
